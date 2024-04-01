fc5ee0f374c208a0841f37003f438d9aceb46664
Mayurrajput97 <mayurrajput7827@gmail.com>
2024-04-01 23:27:37 +0530

    Initial Commit

---
+++ jdbcpr/src/main/java/com/mayur/jdbcpr/App.java
@@ -0,0 +1,13 @@
+package com.mayur.jdbcpr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jdbcpr/src/main/java/db/MyConnection.java
@@ -0,0 +1,28 @@
+package db;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+	static Connection connection = null;
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root", "Mayur");
+			System.out.println("Connection is Done Succusfully ");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch (SQLException e ) {
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+	public static void closeConnection() {
+	}
+	public static void main (String [] args) {
+		MyConnection.getConnection();
+	}
+}
